3436bf75546b2e14292b40b94b30d6f06f38313a
felipe-barria95 <frbarria@uc.cl>
2024-12-08 11:10:28 -0300

    feat(): out transition

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -11,7 +11,9 @@
                     <button 
                         class="text-white mt-8 bg-weather-primary py-2 px-6"
                         @click="$emit('close-modal')"
-                    >Close</button>
+                    >
+                        Close
+                    </button>
                 </div>
             </Transition>
         </div>
@@ -29,13 +31,27 @@ defineProps({
 </script>
 
 <style scoped>
-.modal-outer-enter-active,
-.modal-outer-leave-active {
-    transition: opacity 0.3s cubic-bezier (0.52, 0.02, 0.19, 1.02);
-}
+    .modal-outer-enter-active,
+    .modal-outer-leave-active {
+        transition: opacity 0.3s cubic-bezier (0.52, 0.02, 0.19, 1.02);
+    }
 
-.modal-outer-enter-from,
-.modal-outer-leave-to {
-    opacity: 0;
-}
+    .modal-outer-enter-from,
+    .modal-outer-leave-to {
+        opacity: 0;
+    }
+    .modal-inner-enter-active {
+        transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+    }
+    .modal-inner-leave-active {
+        transition: all 0.3s cubic-bezier (0.52, 0.02, 0.19, 1.02);
+    }
+
+    .modal-inner-enter-from {
+        opacity: 0;
+        transform: scale(0.8);
+    }
+    .modal-inner-leave-to {
+        transform: scale(0.8);
+    }
 </style>
