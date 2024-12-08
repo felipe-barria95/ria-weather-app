9aff2d5dd5a6f2d55f5ad77f6d6aedcc95186f95
felipe-barria95 <frbarria@uc.cl>
2024-12-08 15:36:06 -0300

    feat(): catch errors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,6 +12,12 @@
         class="absolute text-white w-full shadow-md py-2 px-1 top-[66]"
         v-if="mapboxSearchResults"
       >
+        <p v-if="searchError">
+          Error while fetching data. Please try again
+        </p>
+        <p v-if="!serverErron && mapboxSearchResults.length === 0">
+          No results matched
+        </p>
         <li
           v-for="searchResult in mapboxSearchResults"
           class="py-2 cursor-pointer"
@@ -29,19 +35,24 @@
   const searchQuery = ref('');
   const queryTimeout = ref(null);
   const mapboxSearchResults = ref(null);
+  const searchError = ref(null);
 
   const getSearchResults = () => {
     clearTimeout(queryTimeout.value);
     queryTimeout.value = setTimeout(async () => {
       if (searchQuery.value != "") {
-        const result = await axios.get('/api/static/exports/cities_20000.csv');
-        const splitedCSVResult = result.data.split('\n');
-        const filteredResults = splitedCSVResult.filter((row) => row.includes(searchQuery.value)).slice(0, 10);
-        const parsedResults = filteredResults.map((res) => {
-          const sep = res.split(',');
-          return {cityName: sep[1], country: sep[4], lat: sep[5], lon: sep[6]}
-        });
-        mapboxSearchResults.value = parsedResults;
+        try {
+          const result = await axios.get('/api/static/exports/cities_20000.csv');
+          const CSVResultArray = result.data.split('\n');
+          const filteredResults = CSVResultArray.filter((row) => row.includes(searchQuery.value)).slice(0, 10);
+          const parsedResults = filteredResults.map((res) => {
+            const sep = res.split(',');
+            return {cityName: sep[1], country: sep[4], lat: sep[5], lon: sep[6]}
+          });
+          mapboxSearchResults.value = parsedResults;
+        } catch {
+          searchError.value = true;
+        }
         return
       }
       mapboxSearchResults.value = null;
