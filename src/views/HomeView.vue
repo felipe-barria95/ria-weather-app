5b4635f90edeed32301ee2b23a153bb7fe9a817f
felipe-barria95 <frbarria@uc.cl>
2024-12-08 11:56:52 -0300

    feat(): add cities json file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
-<script setup></script>
-
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input 
+        v-model="searchQuery"
+        type="text" 
+        placeholder="Search for a city" 
+        class="py-2 px-1 w-full bg-transparent border-b •focus: border-weather-secondary focus: outline-none focus: shadow-[Opx_1px_0_0_#004E71]"
+      >
+    </div>
+  </main>
 </template>
+
+<script setup>
+  import { ref } from 'vue';
+  const searchQuery = ref('');
+</script>
