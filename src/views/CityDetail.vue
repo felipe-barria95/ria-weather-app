8847700e86d592059b0601d94c386b43cffb38a4
felipe-barria95 <frbarria@uc.cl>
2024-12-09 12:27:20 -0300

    feat(): delete suspense and handle api fetch in city view

--- src/views/CityDetail.vue
+++ src/views/CityDetail.vue
@@ -2,12 +2,12 @@
     <div class="flex flex-col flex-1 items-center">
         <div class="flex flex-col items-center text-white py-12">
             <h1 class="text-4xl mb-2">
-                {{ weatherData.data.city.name }},
-                {{ weatherData.data.city.country }}
+                {{ weatherData.city.name }},
+                {{ weatherData.city.country }}
             </h1>
             <p class="text-sm mb-12">
                 {{
-                    new Date(weatherData.data.current).toLocaleDateString(
+                    new Date(weatherData.current).toLocaleDateString(
                         'en-us',
                         {
                             weekday: 'short',
@@ -17,7 +17,7 @@
                     )
                 }}
                 {{
-                    new Date(weatherData.data.current).toLocaleTimeString(
+                    new Date(weatherData.current).toLocaleTimeString(
                         'en-us',
                         {
                             timeStyle: 'short',
@@ -26,19 +26,19 @@
                 }}
             </p>
             <p class="text-8xl mb-8">
-                {{ Math.round(weatherData.data.list[0].main.temp) }}&deg; C
+                {{ Math.round(weatherData.list[0].main.temp) }}&deg; C
             </p>
             <p>
                 Feels like
-                {{ Math.round(weatherData.data.list[0].main.feels_like) }}&deg;
+                {{ Math.round(weatherData.list[0].main.feels_like) }}&deg;
                 C
             </p>
             <p class="capitalize">
-                {{ weatherData.data.list[0].weather[0].description }}
+                {{ weatherData.list[0].weather[0].description }}
             </p>
             <img
                 class="w-[150px] h-auto"
-                :src="`http://openweathermap.org/img/wn/${weatherData.data.list[0].weather[0].icon}@2x.png`"
+                :src="`http://openweathermap.org/img/wn/${weatherData.list[0].weather[0].icon}@2x.png`"
                 alt=""
             />
         </div>
@@ -52,13 +52,13 @@
                     <div class="flex gap-10 items-center">
                         <div
                             v-for="i in 5"
-                            :key="weatherData.data.list[i].dt"
+                            :key="weatherData.list[i].dt"
                             class="flex flex-col gap-4 items-center"
                         >
                             <p class="whitespace-nowrap">
                                 {{
                                     new Date(
-                                        weatherData.data.list[i].dt * 1000
+                                        weatherData.list[i].dt * 1000
                                     ).toLocaleTimeString('en-US', {
                                         hour: 'numeric',
                                     })
@@ -66,13 +66,13 @@
                             </p>
                             <img
                                 class="w-auto h-[50px] object-cover"
-                                :src="`http://openweathermap.org/img/wn/${weatherData.data.list[i].weather[0].icon}@2x.png`"
+                                :src="`http://openweathermap.org/img/wn/${weatherData.list[i].weather[0].icon}@2x.png`"
                                 alt=""
                             />
                             <p>
                                 {{
                                     Math.round(
-                                        weatherData.data.list[i].main.temp
+                                        weatherData.list[i].main.temp
                                     )
                                 }}&deg; C
                             </p>
@@ -89,13 +89,13 @@
                 <h2 class="mb-4">4 Day Forecast</h2>
                 <div
                     v-for="i in 4"
-                    :key="weatherData.data.list[i].dt"
+                    :key="weatherData.list[i].dt"
                     class="flex items-center"
                 >
                     <p class="flex-1">
                         {{
                             new Date(
-                                weatherData.data.list[8 * i].dt * 1000
+                                weatherData.list[8 * i].dt * 1000
                             ).toLocaleDateString('en-US', {
                                 weekday: 'long',
                             })
@@ -103,18 +103,18 @@
                     </p>
                     <img
                         class="w-[50px] h-[50pxl object-cover"
-                        :src="`http://openweathermap.org/img/wn/${weatherData.data.list[8 * i].weather[0].icon}@2x.png`"
+                        :src="`http://openweathermap.org/img/wn/${weatherData.list[8 * i].weather[0].icon}@2x.png`"
                         alt=""
                     />
                     <div class="flex gap-2 justify-end">
                         <p>
                             &uarr;{{
-                                weatherData.data.list[8 * i].main.temp_min
+                                weatherData.list[8 * i].main.temp_min
                             }}&deg; C
                         </p>
                         <p>
                             &darr;{{
-                                weatherData.data.list[8 * i].main.temp_max
+                                weatherData.list[8 * i].main.temp_max
                             }}&deg; C
                         </p>
                     </div>
@@ -125,34 +125,9 @@
 </template>
 
 <script setup>
-import axios from 'axios'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-const APIKey = '482944e26d320a80bd5e4f23b3de7d1f'
-const getWeatherData = async () => {
-    try {
-        const weatherData = await axios.get(
-            `/weathermapapi/data/2.5/forecast?lat=${route.params.lat}&lon=${route.params.lon}&appid=${APIKey}&units=metric`
-        )
-        await new Promise((res) => setTimeout(res, 1000))
-        const offsetSeconds = weatherData.data.city.timezone
-        const currentDate = new Date()
-        const adjustedDate = new Date(
-            currentDate.getTime() + offsetSeconds * 1000
-        )
-        weatherData.data.current = adjustedDate
-        // weatherData.data.hourly.forEach((hour) => {
-        //     const utc = hour.dt * 1000 + localoffset;
-        //     hour.currentTime = utc + 1000 * weatherData.data. timezone_offset;
-        // });
-        return weatherData
-    } catch (err) {
-        console.log(err)
-    }
-}
-const weatherData = await getWeatherData()
-
+defineProps({
+    weatherData: Object,
+})
 </script>
 
 <style scoped>
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,13 +1,55 @@
 <template>
-    <Suspense>
-        <AsyncCityView />
-        <template #fallback>
-            <CityViewSkeleton />
-        </template>
-    </Suspense>
+    <div v-if="isLoading || !weatherData">
+        <CityViewSkeleton />
+    </div>
+    <div v-else>
+        <CityDetail :weatherData="weatherData"/>
+    </div>
 </template>
 
 <script setup>
 import CityViewSkeleton from './CityViewSkeleton.vue'
-import AsyncCityView from './AsyncCityView.vue'
+import CityDetail from './CityDetail.vue'
+
+import axios from 'axios'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const APIKey = '482944e26d320a80bd5e4f23b3de7d1f'
+const weatherData = ref(null)
+const isLoading = ref(false)
+const route = useRoute()
+const getWeatherData = async (lat, lon) => {
+    isLoading.value = true
+    try {
+        const response = await axios.get(
+            `/weathermapapi/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`
+        )
+        await new Promise((res) => setTimeout(res, 1000))
+        const offsetSeconds = response.data.city.timezone
+        const currentDate = new Date()
+        const adjustedDate = new Date(
+            currentDate.getTime() + offsetSeconds * 1000
+        )
+        response.data.current = adjustedDate
+        weatherData.value = response.data;
+    } catch (err) {
+        console.log(err)
+    } finally {
+        isLoading.value = false
+    }
+}
+
+getWeatherData(route.params.lat, route.params.lon)
+
+
+import { onBeforeRouteUpdate } from 'vue-router'
+
+onBeforeRouteUpdate((to, from, next) => {
+    if (to.params.lat !== from.params.lat || to.params.lon !== from.params.lon) {
+        getWeatherData(to.params.lat, to.params.lon)
+    }
+    next()
+})
+
 </script>
