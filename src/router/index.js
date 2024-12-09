185fddb3b1e0be44812bc08a513e2eeb2d59c9cf
felipe-barria95 <frbarria@uc.cl>
2024-12-09 00:29:31 -0300

    feat(): add separate home and search view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
 import CityView from '../views/CityView.vue'
 
 const router = createRouter({
@@ -9,13 +9,24 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        title: "Home",
+      },
     },
     {
       path: '/weather/:lat/:lon',
       name: 'cityView',
       component: CityView,
+      meta: {
+        title: "Weather",
+      },
     },
   ],
-})
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | RIA Weather App`;
+  next();
+});
 
 export default router
